54ec71a663528c892a7d8b76cdf1712e15a81c58
Carlitos000 <121677820+Carlitos000@users.noreply.github.com>
2023-04-07 20:42:14 +0000

    Define the logic for adding a new task and ass a Set function to save the new data in a local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,32 @@
+//Reference each element
 const task_element = document.getElementById("tasks");
 const task_template = document.getElementById("task-template");
 const new_task_button = document.getElementById("new-task");
 
+//Fetch the existing task items from local storage, convert a JSON string into a javascript array and set the tasks items
 let tasks = getTasks();
 function getTasks() {
     const value = localStorage.getItem("done-app") || "[]";
     return JSON.parse(value);
 
 }
-function setTasks() {
-    const taskJSON = JSON.stringify(tasks);
-    localStorage.getItem()
+function setTasks(task) {
+    const tasksJSON = JSON.stringify(tasks);
+    localStorage.setItem("done-app", tasksJSON);
 
 }
+
+//Defining the logic for adding a new task and add a Set function to save the new tasks in a local storage
+function addInsert() {
+    tasks.unshift({
+        description: "",
+        completed: false});
+    
+    setTasks(tasks);
+
+}
+
+
+
 console.log(tasks);
 
